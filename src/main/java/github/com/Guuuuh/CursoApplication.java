f188f0db4af58d6ed9c6a187b714e8ccec85581d
Guuuuh <gustavo.htl@hotmail.com>
2022-10-10 10:49:04 -0300

    Começo do curso de Spring boot

---
+++ src/main/java/github/com/Guuuuh/CursoApplication.java
@@ -0,0 +1,11 @@
+package github.com.Guuuuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CursoApplication.class, args);
+    }
+}
